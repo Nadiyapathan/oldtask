f8bc26d082e93fad5785103adb89861d56bd3d18
Nadiyapathan <https://github.com/Nadiyapathan/jenkins-gittask.git>
2024-08-30 02:14:31 +0530

    this is java

---
+++ Calculator1.java
@@ -0,0 +1,52 @@
+mport java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Simple Calculator!");
+
+        // Input: First number
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        // Input: Operator
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        // Input: Second number
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result;
+
+        // Perform calculation based on the operator
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator.");
+                break;
+        }
+
+        scanner.close();
+    }
+}
